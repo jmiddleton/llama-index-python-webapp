66208a521ebd1c96e8f41bf84cc134444ff72ed3
Jorge Middleton <jorge.middleton@gmail.com>
2024-04-03 14:10:37 +1100

    changed markdown for Hierarchical NodeParser and AutoMergingRetriever for queries

--- backend/app/engine/__init__.py
+++ backend/app/engine/__init__.py
@@ -1,6 +1,9 @@
 import os
 from app.engine.index import get_index
 from llama_index.postprocessor.flag_embedding_reranker import FlagEmbeddingReranker
+from llama_index.core.postprocessor import SentenceTransformerRerank
+from llama_index.core.retrievers import AutoMergingRetriever
+from llama_index.core.query_engine import RetrieverQueryEngine
 
 top_k = os.getenv("TOP_K", 3)
 
@@ -13,13 +16,11 @@ recursive_chat_engine = None
 recursive_query_engine = None
 
 def get_chat_engine():
-    system_prompt = os.getenv("SYSTEM_PROMPT")
-
     global recursive_chat_engine
     if recursive_chat_engine == None:
         recursive_chat_engine = get_index().as_chat_engine(
             similarity_top_k=5,
-            system_prompt=system_prompt,
+            system_prompt=os.getenv("SYSTEM_PROMPT"),
             node_postprocessors=[reranker],
             chat_mode="condense_plus_context",
             verbose=True
@@ -29,12 +30,30 @@ def get_chat_engine():
 
 def get_query_engine():
 
-    global recursive_query_engine
-    if recursive_query_engine == None:
-        recursive_query_engine = get_index().as_query_engine(
-            similarity_top_k=15,
-            node_postprocessors=[reranker],
-            verbose=True
-        )
+    # global recursive_query_engine
+    # if recursive_query_engine == None:
+    #     recursive_query_engine = get_index().as_query_engine(
+    #         similarity_top_k=15,
+    #         node_postprocessors=[reranker],
+    #         verbose=True
+    #     )
 
-    return recursive_query_engine
+    # return recursive_query_engine
+
+    automerging_retriever = get_index().as_retriever(
+        similarity_top_k=6
+    )
+
+    retriever = AutoMergingRetriever(
+        automerging_retriever, 
+        get_index().storage_context, 
+        verbose=True
+    )
+
+    rerank = SentenceTransformerRerank(top_n=6, model="BAAI/bge-reranker-base")
+
+    auto_merging_engine = RetrieverQueryEngine.from_args(
+        retriever, node_postprocessors=[rerank]
+    )
+
+    return auto_merging_engine
--- backend/app/engine/generate.py
+++ backend/app/engine/generate.py
@@ -1,11 +1,24 @@
 from dotenv import load_dotenv
-from llama_index.core import VectorStoreIndex
-from llama_index.core.node_parser import MarkdownElementNodeParser
+from llama_index.core import (
+    ServiceContext,
+    StorageContext,
+    VectorStoreIndex
+)
+from llama_index.core.node_parser import (
+    HierarchicalNodeParser,
+    get_leaf_nodes,
+    MarkdownElementNodeParser
+)
 from llama_index.core.settings import Settings
 from app.engine.constants import STORAGE_DIR
 from app.engine.loader import get_documents, get_webpages
 from app.settings import init_settings
 import logging
+
+# create the hierarchical node parser w/ default settings
+node_parser = HierarchicalNodeParser.from_defaults(
+    chunk_sizes=[2048, 512, 128]
+)
 
 load_dotenv()
 
@@ -19,14 +32,25 @@ def generate_datasource():
     documents = get_documents()
     #web_documents = get_webpages()
 
-    node_parser = MarkdownElementNodeParser(llm= Settings.llm , num_workers=4)
+    
     nodes = node_parser.get_nodes_from_documents(documents)
-    base_nodes, objects = node_parser.get_nodes_and_objects(nodes)
+    leaf_nodes = get_leaf_nodes(nodes)
 
-    recursive_index = VectorStoreIndex(nodes=base_nodes+objects)
+    storage_context = StorageContext.from_defaults()
+    storage_context.docstore.add_documents(nodes)
+
+    index = VectorStoreIndex(
+        leaf_nodes, storage_context=storage_context
+    )
+
+    # node_parser = MarkdownElementNodeParser(llm= Settings.llm , num_workers=4)
+    # nodes = node_parser.get_nodes_from_documents(documents)
+    # base_nodes, objects = node_parser.get_nodes_and_objects(nodes)
+
+    #index = VectorStoreIndex(nodes=base_nodes+objects)
 
     # store it for later
-    recursive_index.storage_context.persist(STORAGE_DIR)
+    index.storage_context.persist(STORAGE_DIR)
     logger.info(f"Finished creating new index. Stored in {STORAGE_DIR}")
 
 
